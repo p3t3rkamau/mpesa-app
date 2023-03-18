eb6d67821e532c171d66283221052b5bf161453d
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2023-03-18 11:23:01 +0300

    2222222222

--- index.php
+++ index.php
@@ -1,3 +1,93 @@
+<?php
+if(isset($_POST['submit'])){
+
+
+  date_default_timezone_set('Africa/Nairobi');
+
+  # access token
+  $consumerKey = 'E5wzTlk8KUdaycxsUA4xJTOWHNJ9bBRN'; //Fill with your app Consumer Key
+  $consumerSecret = 's7Fk10AWftAy4Agd'; // Fill with your app Secret
+
+  # define the variales
+  # provide the following details, this part is found on your test credentials on the developer account
+  $BusinessShortCode = '174379';
+  $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
+  
+  /*
+    This are your info, for
+    $PartyA should be the ACTUAL clients phone number or your phone number, format 2547********
+    $AccountRefference, it maybe invoice number, account number etc on production systems, but for test just put anything
+    TransactionDesc can be anything, probably a better description of or the transaction
+    $Amount this is the total invoiced amount, Any amount here will be 
+    actually deducted from a clients side/your test phone number once the PIN has been entered to authorize the transaction. 
+    for developer/test accounts, this money will be reversed automatically by midnight.
+  */
+  
+  $PartyA = $_POST['phone']; // This is your phone number, 
+  $AccountReference = '2255';
+  $TransactionDesc = 'Test Payment';
+  $Amount = $_POST['amount'];
+ 
+  # Get the timestamp, format YYYYmmddhms -> 20181004151020
+  $Timestamp = date('YmdHis');    
+  
+  # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+  $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
+
+  # header for access token
+  $headers = ['Content-Type:application/json; charset=utf8'];
+
+    # M-PESA endpoint urls
+  $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+  # callback url
+  $CallBackURL = 'https://petercubolt.000webhostapp.com/callback_url.php';  
+
+  $curl = curl_init($access_token_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($curl, CURLOPT_HEADER, FALSE);
+  curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+  $result = curl_exec($curl);
+  $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+  $result = json_decode($result);
+  $access_token = $result->access_token;  
+  curl_close($curl);
+
+  # header for stk push
+  $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
+
+  # initiating the transaction
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $initiate_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+
+  $curl_post_data = array(
+    //Fill in the request parameters with valid values
+    'BusinessShortCode' => $BusinessShortCode,
+    'Password' => $Password,
+    'Timestamp' => $Timestamp,
+    'TransactionType' => 'CustomerPayBillOnline',
+    'Amount' => $Amount,
+    'PartyA' => $PartyA,
+    'PartyB' => $BusinessShortCode,
+    'PhoneNumber' => $PartyA,
+    'CallBackURL' => $CallBackURL,
+    'AccountReference' => $AccountReference,
+    'TransactionDesc' => $TransactionDesc
+  );
+
+  $data_string = json_encode($curl_post_data);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+  $curl_response = curl_exec($curl);
+  print_r($curl_response);
+
+  echo $curl_response;
+};
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,7 +106,8 @@
       type="text/javascript"
       src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
     ></script>
-    <style>
+                                                                             
+                                                                            <style>
       @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
 
       body {
@@ -69,6 +160,7 @@
         border-radius: 15px;
       }
     </style>
+  
   </head>
   <body oncontextmenu="return false" class="snippet-body">
     <div class="container d-flex justify-content-center">
@@ -87,7 +179,7 @@
         <div class="media mt-3 pl-2">
                           <!--bs5 input-->
 
-            <form class="row g-3" action="./stk_initiate.php" method="POST">
+            <form class="row g-3" action="" method="POST">
             
                 <div class="col-12">
                   <label for="inputAddress" class="form-label">Amount</label>
@@ -116,3 +208,11 @@
     <script type="text/Javascript"></script>
   </body>
 </html>
+
+
+
+
+
+
+
+
